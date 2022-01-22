e2ea71a9e05d71ecf092fa245b506de56975c1fa
harry <harrisongitau3@gmail.com>
2022-01-22 17:05:20 +0300

    Dev: Render app templates

---
+++ app/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,\
+    TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=8, max=30)]
+        )
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=8, max=30)])
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired(), Length(min=8, max=30), EqualTo('password')
+            ])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    body = StringField('Comment', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,53 @@
 from app import app
+from flask import render_template
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, CommentForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    form = CommentForm()
+    return render_template(
+        'index.html',
+        title='Posts',
+        form=form
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template(
+        'login.html',
+        title='Login',
+        form=form
+    )
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template(
+        'register.html',
+        title='Register',
+        form=form
+    )
+
+
+@app.route('/user/<username>')
+def user(username):
+    return render_template(
+        'user.html',
+        title='User',
+        username=username,
+    )
+
+
+@app.route('/edit-profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    return render_template(
+        'edit_profile.html',
+        title='Edit Profile',
+        form=form
+    )
