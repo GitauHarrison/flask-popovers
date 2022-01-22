0db40747d6c2bd25370a9b92d1b4e75f8c761277
harry <harrisongitau3@gmail.com>
2022-01-22 18:08:15 +0300

    Dev: Allow user to edit their profile

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField,\
     TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -27,11 +29,21 @@ class RegistrationForm(FlaskForm):
 
 
 class CommentForm(FlaskForm):
-    body = StringField('Comment', validators=[DataRequired()])
+    body = TextAreaField('Comment', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    about_me = StringField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -105,7 +105,16 @@ def user(username):
 @app.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
     return render_template(
         'edit_profile.html',
         title='Edit Profile',
