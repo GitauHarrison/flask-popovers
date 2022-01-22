bfdbae2eb5652c200c4b3bdd9e1832e492853381
harry <harrisongitau3@gmail.com>
2022-01-22 17:10:42 +0300

    Initialize other dependancies in application instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_moment import Moment
+from flask_migrate import Migrate
 from config import Config
 
 
@@ -7,5 +11,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+moment = Moment(app)
+migrate = Migrate(app, db, render_as_batch=True)
 
 from app import routes, errors
