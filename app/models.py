be89a64ae7e226cfd50c111ec91a4457b559e33b
harry <harrisongitau3@gmail.com>
2022-01-22 17:44:02 +0300

    Feat: Add user authentication

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     __table_name__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +19,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User: {self.username}'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, url_for, redirect, request, flash
+from flask_login import current_user, login_required, login_user, logout_user
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, CommentForm
+from app.models import User
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     form = CommentForm()
     return render_template(
@@ -16,17 +19,44 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Welcome {user.username}')
+        return redirect(url_for('index'))
     return render_template(
         'login.html',
         title='Login',
-        form=form
-    )
+        form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations! Login to continue')
+        return redirect(url_for('login'))
     return render_template(
         'register.html',
         title='Register',
@@ -35,6 +65,7 @@ def register():
 
 
 @app.route('/user/<username>')
+@login_required
 def user(username):
     return render_template(
         'user.html',
@@ -44,6 +75,7 @@ def user(username):
 
 
 @app.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
 def edit_profile():
     form = EditProfileForm()
     return render_template(
