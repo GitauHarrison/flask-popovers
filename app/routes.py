0c774d27179e92187c534e82d13dd46a7eac1d48
harry <harrisongitau3@gmail.com>
2022-01-22 18:42:10 +0300

    Feat: Implement popover

--- app/routes.py
+++ app/routes.py
@@ -128,3 +128,12 @@ def edit_profile():
         title='Edit Profile',
         form=form
     )
+
+
+@app.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template(
+        'user_popup.html',
+        user=user)
